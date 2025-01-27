e6458480a192a969acbfda37833f17ae9864622e
federicotempo <federicotempo98@gmail.com>
2025-01-27 15:02:23 -0300

    Update suppliersRouter.js

--- routes/suppliersRouter.js
+++ routes/suppliersRouter.js
@@ -1,13 +1,14 @@
 const { Router } = require("express");
+const { renderSuppliers } = require("../controllers/suppliersController");
 
 const suppliersRouter = Router();
 
-suppliersRouter.get("/");
+suppliersRouter.get("/", renderSuppliers);
 suppliersRouter.post("/");
 
-suppliersRouter.get("/:id")
-suppliersRouter.put("/:id")
+suppliersRouter.get("/:id");
+suppliersRouter.put("/:id");
 
-suppliersRouter.delete("/:id")
+suppliersRouter.delete("/:id");
 
 module.exports = suppliersRouter;
