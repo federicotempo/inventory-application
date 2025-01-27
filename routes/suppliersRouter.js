0c676338f1eafe16d608f06e4ea3aa7a5a3257fb
federicotempo <federicotempo98@gmail.com>
2025-01-27 09:47:22 -0300

    Add new routes to suppliers

--- routes/suppliersRouter.js
+++ routes/suppliersRouter.js
@@ -3,5 +3,11 @@ const { Router } = require("express");
 const suppliersRouter = Router();
 
 suppliersRouter.get("/");
+suppliersRouter.post("/");
+
+suppliersRouter.get("/:id")
+suppliersRouter.put("/:id")
+
+suppliersRouter.delete("/:id")
 
 module.exports = suppliersRouter;
