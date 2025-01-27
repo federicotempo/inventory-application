1798fdb141fedc5eb22017a1cd7699e63d87659d
federicotempo <federicotempo98@gmail.com>
2025-01-27 08:20:29 -0300

    Create app.js
    -Adds initial configs.

---
+++ app.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const path = require("node:path");
+const indexRouter = require("./routes/indexRouter");
+const categoriesRouter = require("./routes/categoriesRouter");
+
+const app = express();
+const PORT = process.env.PORT || 8000;
+
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
+const assetsPath = path.join(__dirname, "public");
+app.use(express.static(assetsPath));
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use("/", indexRouter);
+app.use("/categories", categoriesRouter);
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
