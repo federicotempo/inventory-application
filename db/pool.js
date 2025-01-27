8884b29e9ee915e2255788e9ecff5914f9a44a23
federicotempo <federicotempo98@gmail.com>
2025-01-27 10:20:48 -0300

    Create pool.js

---
+++ db/pool.js
@@ -0,0 +1,10 @@
+require("dotenv").config();
+const { Pool } = require("pg");
+
+module.exports = new Pool({
+  host: process.env.DATABASE_HOST,
+  user: process.env.DATABASE_USER,
+  database: process.env.DATABASE_NAME,
+  password: process.env.DATABASE_PASSWORD,
+  port: process.env.DATABASE_PORT,
+});
